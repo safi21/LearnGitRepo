648d0c413610a78d4bc12b75b0171e50047dac2a
sappi21 <sappiahmed@gmail.com>
2021-01-30 21:19:29 +0600

    Bubble sort

---
+++ bubbleSort.py
@@ -0,0 +1,9 @@
+l = [24344, 54, 546, 4564, 162, 1654]
+for i in range(len(l) - 1, 0, -1):
+    for j in range(i):
+        if l[j] > l[j + 1]:
+            temp = l[j]
+            l[j] = l[j + 1]
+            l[j + 1] = temp
+
+print(l)
