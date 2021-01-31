88cd5c6fda4cd177bcf7e77506b57963838f7576
sappi21 <sappiahmed@gmail.com>
2021-01-31 16:11:22 +0600

    Modified

--- selectionSort.py
+++ selectionSort.py
@@ -1,3 +1,10 @@
+nn = int(input("Enter the length: "))
+lst = []
+for ii in range(nn):
+    inn = int(input("Enter list element: "))
+    lst.append(inn)
+
+
 def selsort(x):
     for i in range(len(x) - 1):
         min_pos = i
@@ -8,17 +15,4 @@ def selsort(x):
     print(x)
 
 
-lst = [35, 2354, 234345, 23432, 45345, 234, 23, 4, 243, 67, 45, 5, 2234, 2]
 selsort(lst)
-# This is done with simple scripting.
-l = [334, 34, 235, 234, 564, 7, 45]
-for k in range(0, len(l) - 1):
-    mp = k
-    for n in range(k + 1, len(l)):
-        if l[n] < l[mp]:
-            mp = n
-    temp = l[k]
-    l[k] = l[mp]
-    l[mp] = temp
-
-print(l)
