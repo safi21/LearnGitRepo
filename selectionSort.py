e079eae884c453ad82110e37b445d2c542c8acbe
sappi21 <sappiahmed@gmail.com>
2021-01-31 15:54:48 +0600

    selsort with function

--- selectionSort.py
+++ selectionSort.py
@@ -1,3 +1,16 @@
+def selsort(x):
+    for i in range(len(x) - 1):
+        min_pos = i
+        for j in range(k + 1, len(x)):
+            if x[j] < x[min_pos]:
+                min_pos = j
+        x[i], x[min_pos] = x[min_pos], x[i]
+    print(x)
+
+
+lst = [35, 2354, 234345, 23432, 45345, 234, 23, 4, 243, 67, 45, 5, 2234, 2]
+selsort(lst)
+# This is done with simple scripting.
 l = [334, 34, 235, 234, 564, 7, 45]
 for k in range(0, len(l) - 1):
     mp = k
